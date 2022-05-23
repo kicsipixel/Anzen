0118657a8ce9f2b704082f1d9d06d4acf6c09fa1
Szabolcs Tóth <tsz@purzelbaum.hu>
2022-05-23 19:08:14 +0200

    New: Color extension was added.

---
+++ Anzen/ColorExtension.swift
@@ -0,0 +1,14 @@
+//
+//  ColorExtension.swift
+//  Anzen
+//
+//  Created by Szabolcs Tóth on 23.05.2022.
+//  Copyright © 2022 Szabolcs Tóth. All rights reserved.
+//
+
+import SwiftUI
+
+extension Color {
+    static let mountainFig = Color(red: 58/255, green: 61/255, blue: 71/255)
+    static let closedShutter = Color(red: 35/255, green: 36/255, blue: 43/255)
+}
