34505ba320c64502823e54f3d526de84aa25cbb8
Szabolcs Tóth <tsz@purzelbaum.hu>
2022-05-23 10:09:25 +0200

    Initial Commit

---
+++ Anzen/AnzenApp.swift
@@ -0,0 +1,18 @@
+//
+//  AnzenApp.swift
+//  Anzen
+//
+//  Created by Szabolcs Tóth on 23.05.2022.
+//  Copyright © 2022 Szabolcs Tóth. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct AnzenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Anzen/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Anzen
+//
+//  Created by Szabolcs Tóth on 23.05.2022.
+//  Copyright © 2022 Szabolcs Tóth. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
